ead79d2a3fe0fecf0064a649e7a150be254639ff
esh8851 <esh8851@gmail.com>
2024-12-12 16:12:07 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Hello from './Hello'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  let name = "seoul";
   return (
     <>
       <div>
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + {name}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Hello></Hello>
     </>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+  return (
+    <>
+      <h1>Hello React</h1>
+    </>
+  )
+}
+
+export default Hello
